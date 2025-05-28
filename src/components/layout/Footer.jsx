eb1729f01cd7d0ad5407b985b63a26de4a2eab79
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-05-28 17:13:48 +0200

    feat: style footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-primary p-6">
+    <footer className="bg-primary p-6 flex justify-end font-accent font-extralight">
       <p>&copy; {new Date().getFullYear()} Madelen Sletteberg</p>
     </footer>
   );
