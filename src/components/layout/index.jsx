7cae401f70336f63d67b137a10eb0a13b9fbfe44
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-05-28 19:53:33 +0200

    feat: add margin top to main element

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -6,7 +6,7 @@ export default function Layout() {
   return (
     <>
       <Header />
-      <main className="flex flex-col flex-grow">
+      <main className="flex flex-col flex-grow mt-12">
         <Outlet />
       </main>
       <Footer />
