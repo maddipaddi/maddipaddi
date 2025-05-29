8c8bee3d5c3a6d7caeca31d581488c0375afea99
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-05-29 17:34:55 +0200

    feat: make the bookshelf responsive

--- src/components/Bookshelf/index.jsx
+++ src/components/Bookshelf/index.jsx
@@ -2,27 +2,27 @@ import { Link } from "react-router";
 
 export default function Bookshelf() {
   return (
-    <article className="mx-auto font-heading text-2xl font-medium tracking-wide">
+    <article className="mx-auto font-heading text-2xl font-medium tracking-wide px-2">
       <h1 className="text-center">Hi! I'm Maddie, a front-end developer.</h1>
       <p className="text-center ">Browse the bookshelf to learn more.</p>
-      <div className="grid gap-y-8 p-8 mt-8 bg-primary rounded-lg shadow-2xl max-w-4xl border-b-6 border-bookshelf-border">
+      <div className="grid gap-y-8 p-8 mt-8 bg-primary rounded-lg shadow-2xl max-w-md md:max-w-2xl xl:max-w-7xl border-b-6 border-bookshelf-border">
         {/* Shelf 1 */}
-        <div className="bg-secondary grid grid-cols-4 border-t-6 border-b-2 border-bookshelf-border">
+        <div className="bg-secondary grid grid-cols-2 md:grid-cols-4 md:grid-rows-1 border-t-6 border-b-2 border-bookshelf-border">
           <img
             src="/assets/bookspines-3-ChatGPT.png"
-            className="h-32 object-contain translate-y-4"
             alt="Decorated bookspines in golden hues."
+            className="h-32 object-contain translate-y-6"
           />
           <img
             src="/assets/plant-ChatGPT.png"
             alt="Hanging plant in a terracotta pot."
-            className="h-32 object-contain translate-y-8"
+            className="h-32 object-contain translate-y-10 hidden md:flex"
           />
 
           <img
             src="/assets/bookspines-ChatGPT.png"
-            className="h-32 object-contain translate-y-4"
             alt="Decorated bookspines in earth tones."
+            className="h-32 object-contain translate-y-6 hidden md:flex"
           />
           <Link to="/about">
             <img
@@ -34,63 +34,69 @@ export default function Bookshelf() {
         </div>
 
         {/* Shelf 2 */}
-        <div className="bg-secondary grid grid-cols-3 border-t-6 border-b-2 border-bookshelf-border">
-          <div className="p-12 m-4 bg-primary">Project 1</div>
-          <div className="p-12 m-4 bg-primary">Project 2</div>
-          <div className="p-12 m-4 bg-primary">Project 3</div>
+        <div className="bg-secondary grid grid-cols-1 md:grid-cols-3 border-t-6 border-b-2 border-bookshelf-border">
+          <div className="p-12 m-4 bg-primary">
+            <h2>Project 1</h2>
+          </div>
+          <div className="p-12 m-4 bg-primary">
+            <h2>Project 2</h2>
+          </div>
+          <div className="p-12 m-4 bg-primary">
+            <h2>Project 3</h2>
+          </div>
         </div>
 
         {/* Shelf 3 */}
-        <div className="bg-secondary grid grid-cols-4 border-t-6 border-b-2 border-bookshelf-border">
+        <div className="bg-secondary grid grid-cols-2 md:grid-cols-4 border-t-6 border-b-2 border-bookshelf-border">
           <img
             src="/assets/microscope-ChatGPT.png"
-            className="h-32 object-contain translate-y-4"
             alt="Old tomey bronze microscope."
+            className="h-32 object-contain translate-y-4 hidden md:flex"
           />
           <Link to="/resume">
             <img
               src="/assets/typewriter-ChatGPT.png"
-              className="h-32 object-contain translate-y-6"
               alt="Brown typewriter with a paper loaded."
+              className="h-32 object-contain translate-y-6"
             />
           </Link>
 
           <img
             src="/assets/lantern-ChatGPT.png"
-            className="h-32 object-contain translate-y-3"
             alt="Victorian looking gothic lantern."
+            className="h-32 object-contain translate-y-3 hidden md:flex"
           />
           <img
             src="/assets/bookspines-2-ChatGPT.png"
-            className="h-32 object-contain translate-y-5"
             alt="Old looking browntoned bookspines."
+            className="h-32 object-contain translate-y-6"
           />
         </div>
 
         {/* Shelf 4 */}
-        <div className="bg-secondary grid grid-cols-4 border-t-6 border-b-2 border-bookshelf-border">
+        <div className="bg-secondary grid grid-cols-2 md:grid-cols-4 border-t-6 border-b-2 border-bookshelf-border">
           <img
             src="/assets/bookspines-4-ChatGPT.png"
-            className="h-32 object-contain translate-y-4"
             alt="Decorated long row of bookspines."
+            className="h-32 object-contain translate-y-6 hidden md:flex"
           />
 
           <img
             src="/assets/books-n-fossil-ChatGPT.png"
-            className="h-32 object-contain translate-y-4"
             alt="Books laying down with a fossil on top."
+            className="h-32 object-contain translate-y-4 hidden md:flex"
           />
           <Link to="/contact">
             <img
               src="/assets/rotary-phone-ChatGPT.png"
-              className="h-32 object-contain translate-y-3"
               alt="Old timey dark brown rotary phone."
+              className="h-32 object-contain translate-y-3"
             />
           </Link>
           <img
             src="/assets/plant-3-ChatGPT.png"
-            className="h-32 object-contain translate-y-4"
             alt="Green plant in a terracotta pot."
+            className="h-32 object-contain translate-y-4"
           />
         </div>
       </div>
