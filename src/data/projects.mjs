010e07d13cfa6f20f9853c8d75018dc28d885fa3
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-06-03 14:56:00 +0200

    feat: add data to clicketycart and auction house projects

--- src/data/projects.mjs
+++ src/data/projects.mjs
@@ -9,23 +9,48 @@ export const projects = [
     title: "ClicketyCart",
     icon: CCLogo,
     blurb: "An e-commerce prototype focused on accessibility and performance.",
-    collab: "",
+    collab:
+      "This project is a collaboration between Herman Hylland and myself (Madelen Sletteberg).",
+    welcome:
+      "At ClicketyCart, we believe shopping should be as quick and seamless as a single click. No delays, no hassle—just instant access to the products you love. With a sleek, turbo-charged checkout process and lightning-fast delivery, we take online shopping to the next level. Whether you're browsing on the go or making last-minute buys, ClicketyCart gets it done faster than you can say 'add to cart.'",
     about:
-      "This project was built using React and Tailwind...This project was built using React and TailwindThis project was built using React and TailwindThis project was built using React and Tailwind",
+      "This project is the frontend implementation of a fictional e-commerce store, built on an existing backend API provided by Noroff. The aim is to bring the user interface to life with a fun, retro design and modern React functionality.",
+    features: [
+      "The Homepage has a list of all the products. There is a look-ahead search bar that filters products when typing in a product name. Clicking on a product takes a user to an individual product page.",
+      "The individual product page displays data for a single product. There is an Add to cart button which, upon clicking, adds the product to the cart.",
+      "Clicking on the Cart icon will load the Cart page, which will list all of the products as well as a total. The Cart page has a Checkout button. Clicking this Checkout button then goes to a Checkout success page.",
+      "The Checkout success page displays a message to the user notifying them that their order was successful.",
+    ],
     techStack: ["React", "Tailwind", "Vite"],
-    repo: "https://github.com/maddie/clicketycart",
-    live: "https://clicketycart.live",
+    reflection:
+      "Here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow!",
+    repo: "https://github.com/maddipaddi/frameworks-CA",
+    live: "https://clicketycart.netlify.app",
   },
   {
     id: "auctionhouse",
     title: "Auction House",
     icon: AHLogo,
     blurb: "An online auctions app focused on buying and selling luxury items.",
-    collab: "",
-    about: "This project was built using React and Tailwind...",
+    collab:
+      "This project is a collaboration between Veronika Aas and myself (Madelen Sletteberg).",
+    welcome:
+      "Auction House is an exclusive platform for buying and selling luxury items, specializing in watches, handbags, bags, and jewelry. Our mission is to create a refined, user-friendly experience that matches the elegance of the products featured on our platform.",
+    about:
+      "This project is the frontend implementation of Auction House, built on an existing backend API provided by Noroff. The API handles user management, auction listings, and bidding functionality, while this frontend will bring the user interface to life with a luxurious, responsive design.",
+    features: [
+      "Users can register using an email address ending in @stud.noroff.no.",
+      "As a registered user, you can update your profile banner, profile picture, and bio.",
+      "A registered user can create a listing that includes: title, description, deadline date, category, and images.",
+      "As a registered user, you can place bids on other users' listings.",
+      "Visitors who are not registered can browse listings but cannot place bids or view profiles.",
+      "The Wishlist feature is a proof of concept. It is implemented using local storage since the API does not support this functionality or custom fields.",
+    ],
     techStack: ["Vanilla JS", "Tailwind", "Vite"],
-    repo: "https://github.com/maddie/clicketycart",
-    live: "https://clicketycart.live",
+    reflection:
+      "Here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow - here are all my reflections wow!",
+    repo: "https://github.com/maddipaddi/FED2-semester-project-2",
+    live: "https://luxauctionhouse.netlify.app",
   },
   {
     id: "holidaze",
