9cc3828185601f3720b5898b08feb8eead84f896
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-06-03 09:56:09 +0200

    chore: change hex color styles to theme colors

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -5,7 +5,7 @@ export default function ProjectCard({ title, imageSrc, alt, description, to }) {
     <div className="w-full max-w-xs perspective p-2 mx-auto ">
       <div className="relative w-full h-48 transition-transform duration-700 transform-style-preserve-3d group hover:rotate-y-180">
         {/* Front Side */}
-        <div className=" absolute inset-0 bg-[#fef9f0] border-2 border-[#c0a36e] rounded-lg shadow-md flex flex-col items-center justify-center backface-hidden px-4  bg-bookshelf-border max">
+        <div className=" absolute inset-0 border-2 border-accent-gold rounded-lg shadow-md flex flex-col items-center justify-center backface-hidden px-4  bg-bookshelf-border max">
           <div className="max-h-32">
             <img
               src={imageSrc}
@@ -16,7 +16,7 @@ export default function ProjectCard({ title, imageSrc, alt, description, to }) {
         </div>
 
         {/* Back Side */}
-        <div className="absolute inset-0 bg-[#fef9f0] border-2 border-[#c0a36e] rounded-lg shadow-md transform rotate-y-180 flex flex-col justify-center items-center px-4 text-center backface-hidden bg-background">
+        <div className="absolute inset-0 bg-background border-2 border-accent-gold rounded-lg shadow-md transform rotate-y-180 flex flex-col justify-center items-center px-4 text-center backface-hidden">
           <Link
             to={to}
             className="text-black hover:text-accent2 font-semibold"
