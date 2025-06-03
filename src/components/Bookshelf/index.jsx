c862c2d6f62c467864ea59363f389e54bbd46845
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-06-03 12:15:37 +0200

    feat: add descriptions to project cards

--- src/components/Bookshelf/index.jsx
+++ src/components/Bookshelf/index.jsx
@@ -43,21 +43,21 @@ export default function Bookshelf() {
             title="ClicketyCart"
             imageSrc={ClicketyCartLogo}
             alt="Shopping cart racing over the words 'Clickety Cart'"
-            description="This is not the actual description. We are just checking the visuals. Replace later."
+            description="An e-commerce prototype focused on accessibility and performance."
             to="/projects/clicketycart"
           />
           <ProjectCard
             title="Auction House"
             imageSrc={AuctionHouseLogo}
             alt="Diamond shaped logo with the words 'Auction House' to the right."
-            description="This is not the actual description. We are just checking the visuals. Replace later."
+            description="An online auctions app focused on buying and selling luxury items."
             to="/projects/auctionhouse"
           />
           <ProjectCard
             title="Holidaze"
             imageSrc={HolidazeLogo}
             alt="Log cabin with a smoking pipe over the words 'Holidaze'."
-            description="This is not the actual description. We are just checking the visuals. Replace later."
+            description="An airbnb-esque prototype built around hosting and renting cabins."
             to="/projects/holidaze"
           />
         </div>
