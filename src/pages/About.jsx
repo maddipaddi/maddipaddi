182d6512614cdcc0f0e208f265e284ef0fc452b6
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-07-08 13:47:36 +0200

    feat: adjust content on about me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -24,34 +24,24 @@ export default function About() {
           <ul className="flex flex-wrap gap-2 bg-background text-copy p-8 md:max-w-xl">
             <li className="flex gap-2 p-2">
               <CircleSmall className="flex-shrink-0" /> My name is Madelen
-              (Maddie) Sletteberg
+              Sletteberg
             </li>
             <li className="flex gap-2 p-2">
-              <CircleSmall className="flex-shrink-0" /> 28 years old
+              <CircleSmall className="flex-shrink-0" /> 28 years old - young
+              ish, neck injuries now come from sleeping wrong rather than
+              headbanging
             </li>
             <li className="flex gap-2 p-2">
-              <CircleSmall className="flex-shrink-0" /> Fun
+              <CircleSmall className="flex-shrink-0" /> Fun(ny) - most of my
+              friends agree with this
             </li>
             <li className="flex gap-2 p-2">
-              <CircleSmall className="flex-shrink-0" /> Professional
+              <CircleSmall className="flex-shrink-0" /> Detail-oriented - which
+              I hope you can tell from this portfolio
             </li>
             <li className="flex gap-2 p-2">
-              <CircleSmall className="flex-shrink-0" /> Detail-oriented
-            </li>
-            <li className="flex gap-2 p-2">
-              <CircleSmall className="flex-shrink-0" /> Adventurous
-            </li>
-            <li className="flex gap-2 p-2">
-              <CircleSmall className="flex-shrink-0" /> Communicative,
-              diplomatic, and team player
-            </li>
-            <li className="flex gap-2 p-2">
-              <CircleSmall className="flex-shrink-0" /> Thrive on creativity and
-              problem-solving
-            </li>
-            <li className="flex gap-2 p-2">
-              <CircleSmall className="flex-shrink-0" /> Love bringing order to
-              chaos with code
+              <CircleSmall className="flex-shrink-0" /> Adventurous - check out
+              my hobbies
             </li>
           </ul>
         </div>
@@ -64,21 +54,24 @@ export default function About() {
         </div>
       </div>
       <div className="bg-primary h-16 border-b-4 border-bookshelf-border"></div>
-      <div className="flex flex-wrap justify-center mb-8 gap-8 items-center px-4">
+      <div className="flex flex-wrap justify-center items-stretch mb-8 gap-8 items-center px-4">
         <div className="bg-background text-copy p-8 mt-8 lg:max-w-xl xl:max-w-lg">
-          <h2 className="mb-2 font-medium">Education:</h2>
+          <h2 className="mb-2 font-medium">Professionally, I'm...</h2>
           <ul className="flex flex-wrap gap-2">
             <li className="flex gap-2 p-2">
-              <GraduationCap className="flex-shrink-0" /> A bachelor degree in
-              linguistics
+              <CircleSmall className="flex-shrink-0" /> Communicative,
+              diplomatic, and a team player (several of my projects have been
+              successful collaborations with classmates)
             </li>
             <li className="flex gap-2 p-2">
-              <GraduationCap className="flex-shrink-0" /> A bachelor degree in
-              nursing
+              <CircleSmall className="flex-shrink-0" /> Thriving on creativity
+              and problem-solving (anyone who's worked with CSS can attest to
+              the need to enjoy frustration to work in frontend)
             </li>
             <li className="flex gap-2 p-2">
-              <Laptop className="flex-shrink-0" /> A two-year front-end
-              development degree
+              <CircleSmall className="flex-shrink-0" /> Bringing order to chaos
+              with code (I'm the one on the team who installs prettier, sets up
+              pre-commit hooks and makes everyone follow commit guidelines)
             </li>
           </ul>
         </div>
